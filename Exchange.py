c023a903a41eff8cae806b405f6939531997bcf4
Archershonor <archersgonor@gmail.com>
2023-02-27 02:50:12 +0200

    Use Black

--- Exchange.py
+++ Exchange.py
@@ -7,31 +7,33 @@ from Parser import ExchangeAPI
 from flask_crontab import Crontab
 
 
-
-app = Flask (__name__)
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://odoo:odoo@localhost/exchange'
+app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://odoo:odoo@localhost/exchange"
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 Parser = ExchangeAPI()
 crontab = Crontab(app)
 
+
 class currency(db.Model):
-    __tablename__ = 'currency'
-    id = db.Column('id', db.Integer, primary_key = True)
+    __tablename__ = "currency"
+    id = db.Column("id", db.Integer, primary_key=True)
     code = db.Column(db.String(6), nullable=False)
     name = db.Column(db.String(100))
-    currency_value_ids = db.relationship('values', backref='currency')
+    currency_value_ids = db.relationship("values", backref="currency")
 
     def __repr__(self):
         return f"<{self.code}>"
 
 
 class values(db.Model):
-    __tablename__ = 'values'
-    id = db.Column(db.Integer, primary_key = True)
+    __tablename__ = "values"
+    id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
-    currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'))
+    currency_id = db.Column(db.Integer, db.ForeignKey("currency.id"))
 
 
 class currencySchema(ma.SQLAlchemySchema):
@@ -39,15 +41,16 @@ class currencySchema(ma.SQLAlchemySchema):
         model = currency
         include_relationship = True
         load_instance = True
-    
-    id   = ma.auto_field()
+
+    id = ma.auto_field()
     name = ma.auto_field()
     code = ma.auto_field()
     currency_value_ids = ma.List(ma.HyperlinkRelated("show_all"))
-    # currency_value_ids = ma.auto_field() 
-    # in current last version of marshmallow_sqlalchemy 
+    # currency_value_ids = ma.auto_field()
+    # in current last version of marshmallow_sqlalchemy
     # SQLAlchemyAutoSchema can`t work with relationship fields
     # But we can use SQLAlchemySchema with list of HyperlinkRelated
+
 
 class valuesSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -56,57 +59,60 @@ class valuesSchema(ma.SQLAlchemyAutoSchema):
         load_instance = True
 
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def show_all():
-    if request.form.get('search'):
-        return show_one_intime(request.form.get('search'))
+    if request.form.get("search"):
+        return show_one_intime(request.form.get("search"))
         # if the search function needs to search the db, uncomment this block of code
-        # return  render_template("insex.html", 
+        # return  render_template("insex.html",
         #         currency_list=currency.query.like(code = request.form.get('search')))
-    elif request.form.get('get_values'):
+    elif request.form.get("get_values"):
         parce_now()
 
-    schema =  currencySchema()
-    exch = db.session.scalars(db.select(currency)).all() # Example of marshmallow deserialize
+    schema = currencySchema()
+    exch = db.session.scalars(
+        db.select(currency)
+    ).all()  # Example of marshmallow deserialize
     currency_list = schema.dump(exch, many=True)
     print(currency_list)
-    return render_template('index.html', currency_list = currency_list )
+    return render_template("index.html", currency_list=currency_list)
     # return render_template('index.html', currency_list = currency.query.all() ) # Varriant without marshmallow
 
 
-@app.route('/currency-<code>', methods = ['GET', 'POST'])
+@app.route("/currency-<code>", methods=["GET", "POST"])
 def show_one(code):
     exch = currency.query.filter_by(code=code).first()
-    vals  = values.query.filter_by(currency_id=exch.id).all()
+    vals = values.query.filter_by(currency_id=exch.id).all()
     print(exch.currency_value_ids)
-    return render_template('currency.html', currency_list=vals, code=code)
+    return render_template("currency.html", currency_list=vals, code=code)
 
-@app.route('/intime/<code>', methods = ['GET', 'POST'])
+
+@app.route("/intime/<code>", methods=["GET", "POST"])
 def show_one_intime(code):
     exch = Parser.get_one_exchange_value(code)
-    if exch.get('rates'):
-        base=exch.get('base')
-        for key,val in exch.get('rates').items():
+    if exch.get("rates"):
+        base = exch.get("base")
+        for key, val in exch.get("rates").items():
             cur, value = key, val
     else:
-        base='USD'
-        cur=code
-        value="{} is not a code of currency".format(cur)
-    return render_template('show_one.html', base=base, cur=cur, value=value )
+        base = "USD"
+        cur = code
+        value = "{} is not a code of currency".format(cur)
+    return render_template("show_one.html", base=base, cur=cur, value=value)
 
 
 @app.cli.command
 @crontab.job(minute="1", hour="0")
 def planning_updator():
-    print('crontab running ...')
-    app.logger.info('crontab running ...')
+    print("crontab running ...")
+    app.logger.info("crontab running ...")
 
 
 def parce_now():
     ex_dict = Parser.get_exchange_values()
     if ex_dict:
-        _date = ex_dict.get('date')
-        for key, value in ex_dict.get('rates').items():
+        _date = ex_dict.get("date")
+        for key, value in ex_dict.get("rates").items():
             print(key, value)
             exch = currency.query.filter_by(code=key).first()
             if not exch:
@@ -116,16 +122,16 @@ def parce_now():
                 exch = currency.query.filter_by(code=key).first()
             val = values.query.filter_by(date=_date, currency_id=exch.id).first()
             if not val:
-                vall = values(currency_id=exch.id,value=value,date=_date)
+                vall = values(currency_id=exch.id, value=value, date=_date)
                 db.session.add(vall)
                 db.session.commit()
                 val = values.query.filter_by(date=_date, currency_id=exch.id).first()
             elif val.value != value:
-                val.value=value
+                val.value = value
                 db.session.commit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug = True)
+    app.run(debug=True)
